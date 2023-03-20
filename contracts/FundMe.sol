5d53c0b255eae1a80e02e6ad6a7dc1438bb9b1e6
Niharika Singh <niharika.3297@gmail.com>
2023-03-20 15:44:48 +0100

    Init project

---
+++ contracts/FundMe.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import "./PriceConverter.sol";
+
+error NotOwner();
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public constant MINIMUM_USD = 50 * 1e18;
+
+    address[] public depositors;
+    mapping(address => uint256) public addressToAmountFunded;
+
+    address public immutable i_owner;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate() >= MINIMUM_USD,
+            "Mininum dollar amount is 50."
+        );
+        depositors.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        // require (msg.sender == owner, "Only the owner can call this function");
+
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < depositors.length;
+            funderIndex++
+        ) {
+            addressToAmountFunded[depositors[funderIndex]] = 0;
+        }
+
+        depositors = new address[](0);
+
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call failed");
+    }
+
+    modifier onlyOwner() {
+        // require (msg.sender == i_owner, "Only the owner can call this function");
+        if (msg.sender != i_owner) {
+            revert NotOwner();
+        }
+        _;
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+}
