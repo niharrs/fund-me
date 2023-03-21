92ac7e451743f72eb9855dbc50d9eedb20388306
Niharika Singh <niharika.3297@gmail.com>
2023-03-21 13:49:24 +0100

    Deploy mock contract using hardhat-deploy

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,14 +1,23 @@
 const networkConfig = {
-    11155111: {
-        name: "sepolia",
-        ethUsdPriceFeedAddress: "0x694AA1769357215DE4FAC081bf1f309aDC325306"
-    },
-    137: {
-        name: "polygon-mumbai",
-        ethUsdPriceFeedAddress: "0x0715A7794a1dc8e42615F059dD6e406A6594651A"
-    }
-}
+  11155111: {
+    name: "sepolia",
+    ethUsdPriceFeedAddress: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
+  },
+  137: {
+    name: "polygon-mumbai",
+    ethUsdPriceFeedAddress: "0x0715A7794a1dc8e42615F059dD6e406A6594651A",
+  },
+};
+
+const developmentChains = ["hardhat", "localhost"];
+
+const DECIMALS = "8";
+
+const INITIAL_ANSWER = "200000000000";
 
 module.exports = {
-    networkConfig
-}
+  networkConfig,
+  developmentChains,
+  DECIMALS,
+  INITIAL_ANSWER,
+};
